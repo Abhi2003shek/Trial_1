686ef71b38cd34db2b487be475c418c5c27c54a0
Abhi2003shek <azrack11@gmail.com>
2023-06-01 19:12:35 +0530

    rolling a dice added

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include<cmath>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 
 int main() {
+	//Area of circle
 	cout << "Enter the radius of a circle: ";
 	int radius;
 	cin >> radius;
-	double AreaCircle = 3.14 * pow(radius, 2);
-	cout << "Area of the Circle: " << AreaCircle;
+	double AreaCircle = 3.14 * pow(radius, 2); //pi*r^2
+	cout << "Area of the Circle: " << AreaCircle << endl;
+	
+	
+	//Random roll a dice
+	srand(time(nullptr));
+	int Dice = (rand() % (6-1+1))+1;  //[rand()%(maxTerm-minTerm+1]+1
+	cout << "Rolling a Dice : " << Dice << endl;
+
+
+
 	return 0;
-	//Area of circle
 }
